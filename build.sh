e52598e5ab0b4f2343b219cafce4dcacc83e2f8b
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2016-03-10 15:58:19 +0100

    Toevoegen van licenties. Refs #8

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 rapper ontology.ttl -i turtle -o rdfxml-abbrev > ontology.rdf
 rapper ontology.ttl -i turtle -o ntriples > ontology.nt
 # curl --form "mimetypeFile=text/turtle" --form "datafile=@ontology.ttl" -X POST -L http://ontorule-project.eu/parrot/parrot > ontology.html
+rapper licences.ttl -i turtle -o rdfxml-abbrev > licences.rdf
+rapper licences.ttl -i turtle -o ntriples > licences.nt
